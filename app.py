9af3d82f99a04dce4cf2b3eeac57cf60022f9102
Phil Waterworth <i03404975@uk.endress.com>
2021-05-14 16:58:07 +0100

    changed app slightly

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-
-
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # This turns off Flasks modification tracker for SQLAlchemy, saves memory. SQLAlchemy has an inbuilt modification tracker.
@@ -28,9 +25,8 @@ api.add_resource(StoreList, "/stores")
 api.add_resource(UserRegister, "/register")
 
 
-
 if __name__ == '__main__':
-
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug = True)
 
